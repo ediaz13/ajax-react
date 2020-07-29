5ccc05c2c1c72fc4d39fd82ddba8be8305c46e50
EMANUEL DIAZ <emadiaz@santandertecnologia.com.ar>
2020-07-28 22:08:32 -0300

    173. Setting a Default Global Configuration for Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
+import Axios from 'axios';
 import Blog from './containers/Blog/Blog';
+
+Axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+Axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+Axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+Axios.interceptors.request.use(request => {
+  console.log(request);
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+Axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
 
 class App extends Component {
   render() {
