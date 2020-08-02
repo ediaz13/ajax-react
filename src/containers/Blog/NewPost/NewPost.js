e74b3a9de2a865f00c91fba42a7e1ff098f1aa3e
EMANUEL DIAZ <emadiaz@santandertecnologia.com.ar>
2020-08-02 00:20:51 -0300

    211. Conditional Redirects

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 import Axios from "axios";
 import './NewPost.css';
+import { Redirect } from 'react-router-dom';
+
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Emolino'
+        author: 'Emolino',
+        submitted: false
     }
 
     componentDidMount() {
@@ -21,26 +24,33 @@ class NewPost extends Component {
             author: this.state.author
         };
         Axios.post('/posts', post)
-        .then(response => {
-            console.log(response);
-        });
+            .then(response => {
+                console.log(response);
+                this.setState({ submitted: true });
+            });
 
     }
 
-    render () {
+    render() {
+        let redirect = null;
+        if (this.state.submitted) {
+            redirect = <Redirect to="/posts" />
+
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
                     <option value="Noelina">Noelina</option>
                     <option value="Emolino">Emolino</option>
                 </select>
-                <button onClick= {this.postDataHandler}>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
